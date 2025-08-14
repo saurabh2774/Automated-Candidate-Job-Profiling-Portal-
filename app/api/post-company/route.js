3ee440c4e3bf836754bf593f0e012f7ebe65899b
Rohit Dhule <dhulerohit970@gmail.com>
2025-08-14 15:15:54 +0530

    Update route.js

--- app/api/post-company/route.js
+++ app/api/post-company/route.js
@@ -1,26 +1,17 @@
-import { MongoClient } from 'mongodb';
-
-const uri = process.env.MONGODB_URI;
+import clientPromise from '@/lib/mongodb';
+import { NextResponse } from 'next/server';
 
 export async function POST(request) {
     try {
-        const client = await MongoClient.connect(uri);
-        const db = client.db();
+        const client = await clientPromise;
+        const db = client.db("resumePortal");
         const data = await request.json();
 
         const result = await db.collection('companyData').insertOne(data);
 
-        client.close();
-
-        return new Response(JSON.stringify({ message: 'Data inserted successfully', id: result.insertedId }), {
-            headers: { 'Content-Type': 'application/json' },
-            status: 201
-        });
+        return NextResponse.json({ message: 'Data inserted successfully', id: result.insertedId }, { status: 201 });
 
     } catch (error) {
-        return new Response(JSON.stringify({ message: 'Error inserting data', error: error.message }), {
-            headers: { 'Content-Type': 'application/json' },
-            status: 500
-        });
+        return NextResponse.json({ message: 'Error inserting data', error: error.message }, { status: 500 });
     }
 }
