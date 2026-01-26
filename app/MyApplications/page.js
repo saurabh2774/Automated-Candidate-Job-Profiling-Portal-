405cf9e56b59c514db1fa586e8beb0faef063b8b
Rohit Dhule <dhulerohit970@gmail.com>
2026-01-26 21:19:53 +0530

    Remove unused Image import
    Removed unused Image import from MyApplications page.

--- app/MyApplications/page.js
+++ app/MyApplications/page.js
@@ -2,7 +2,6 @@
 
 import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
-import Image from 'next/image';
 import { FiCheckCircle, FiShare2, FiBell } from 'react-icons/fi';
 
 const MyApplicationsPage = () => {
